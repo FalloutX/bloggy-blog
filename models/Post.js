07fa74c4ac18b10178fdbc6c1cac2c7107d02dff
inderjitsidhu <inderjit.sidhu@cube26.com>
2017-06-07 14:08:28 +0530

    Add DELETE, GET, PUT api for the Post Model.

--- models/Post.js
+++ models/Post.js
@@ -11,4 +11,13 @@ const postSchema = new Schema({
   last_modified_at: {type: Number, required: true}
 })
 
+postSchema.methods.isAuthor = function (user) {
+  return this.authorId === user._id
+}
+postSchema.methods.updateData = function (data) {
+  this.title = data.title || this.title
+  this.content = data.content || this.content
+  this.last_modified_at = data.last_modified_at || this.last_modified_at
+}
+
 export default mongoose.model('Post', postSchema)
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,12 @@
 import passport from 'passport'
+import mongoose from 'mongoose'
 
 import Post from '../../models/Post'
+
+
+function isValidObjectID (string) {
+  return mongoose.Types.ObjectId.isValid(string)
+}
 
 
 export default function posts (router) {
@@ -43,6 +49,83 @@ export default function posts (router) {
       .catch(err => {
         res.status(400).json(err)
       })
+    }
+  )
+
+  router.get('/posts/:id',
+    (req, res) => {
+      let { id } = req.params
+      if (!isValidObjectID(id)) {
+        res.status(404).json({info: 'not found'})
+      }
+
+      Post.findById(id)
+        .exec()
+        .then((post) => {
+          if (post) res.json(post)
+          else res.status(404).json({info: 'not found'})
+        })
+        .catch((err) => {
+          res.status(500).json(err)
+        })
+    }
+  )
+
+  router.put('/posts/:id',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+      let { id } = req.params
+      if (!isValidObjectID(id)) {
+        res.status(404).json({info: 'not found'})
+      }
+
+      Post.findById(id)
+        .exec()
+        .then((post) => {
+          if (post && post.isAuthor(req.user)) {
+            const data = Object.assign({}, req.body, {
+              last_modified_at: (new Date()).getTime()
+            })
+            post.updateData(data)
+            return post.save()
+          } else {
+            res.status(404).json({info: 'not found'})
+          }
+        })
+        .then((post) => {
+          console.log('Post Updated')
+          res.json(post)
+        })
+        .catch((err) => {
+          res.status(500).json(err)
+        })
+    }
+  )
+
+  router.delete('/posts/:id',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+      let { id } = req.params
+      if (!isValidObjectID(id)) {
+        res.status(404).json({info: 'not found'})
+      }
+
+      Post.findById(id)
+        .exec()
+        .then((post) => {
+          if (post && post.isAuthor(req.user)) {
+            return post.remove()
+          } else {
+            res.status(404).json({info: 'not found'})
+          }
+        })
+        .then((post) => {
+          console.log('Post Removed')
+          res.json(post)
+        })
+        .catch((err) => {
+          res.status(500).json(err)
+        })
     }
   )
 }
